1cd7b5acd1abca2809e7af10692902ed07380779
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2019-12-12 11:32:25 +0100

    feat(exception) New `runtime_error` function.
    This function helps to create a JNI error easily.

--- src/exception.rs
+++ src/exception.rs
@@ -1,16 +1,21 @@
-use jni::{errors::Error as JniError, JNIEnv};
+pub use jni::errors::Error;
+use jni::{errors::ErrorKind, JNIEnv};
 use std::thread;
 
-pub(crate) fn unwrap_or_throw<T>(env: &JNIEnv, result: thread::Result<Result<T, JniError>>) -> T
+pub fn runtime_error(message: String) -> Error {
+    Error::from_kind(ErrorKind::Msg(message))
+}
+
+pub fn unwrap_or_throw<T>(env: &JNIEnv, result: thread::Result<Result<T, Error>>) -> T
 where
     T: Default,
 {
     match result {
         Ok(result) => match result {
             Ok(result) => result,
-            Err(jni_error) => {
+            Err(error) => {
                 if !env.exception_check().unwrap() {
-                    env.throw_new("java/lang/RuntimeException", &jni_error.to_string())
+                    env.throw_new("java/lang/RuntimeException", &error.to_string())
                         .expect("Cannot throw an `java/lang/RuntimeException` exception.");
                 }
 
