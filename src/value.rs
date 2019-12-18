fb0262d944021016666ab41c4aa40a65282dfaef
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2019-12-18 13:28:35 +0100

    feat(value) Remove the `non_null` macro + formatting.

--- src/value.rs
+++ src/value.rs
@@ -12,41 +12,34 @@ const LONG_CLASS: &str = "java/lang/Long";
 const FLOAT_CLASS: &str = "java/lang/Float";
 const DOUBLE_CLASS: &str = "java/lang/Double";
 
-macro_rules! non_null {
-    ( $obj:expr, $ctx:expr ) => {
-        if $obj.is_null() {
-            return Err(ErrorKind::NullPtr($ctx).into());
-        } else {
-            $obj
-        }
-    };
-}
-
 impl TryFrom<(&JNIEnv<'_>, JObject<'_>)> for Value {
     type Error = Error;
 
-    fn try_from((env, obj): (&JNIEnv, JObject)) -> Result<Self, Self::Error> {
-        non_null!(obj, "to_value obj argument");
-        if env.is_instance_of(obj, INT_CLASS).unwrap_or(false) {
+    fn try_from((env, jobject): (&JNIEnv, JObject)) -> Result<Self, Self::Error> {
+        if jobject.is_null() {
+            return Err(ErrorKind::NullPtr("`try_from` receives a null object").into());
+        }
+
+        if env.is_instance_of(jobject, INT_CLASS).unwrap_or(false) {
             Ok(Value {
-                value: WasmValue::I32(env.call_method(obj, "intValue", "()I", &[])?.i()?),
+                value: WasmValue::I32(env.call_method(jobject, "intValue", "()I", &[])?.i()?),
             })
-        } else if env.is_instance_of(obj, LONG_CLASS).unwrap_or(false) {
+        } else if env.is_instance_of(jobject, LONG_CLASS).unwrap_or(false) {
             Ok(Value {
-                value: WasmValue::I64(env.call_method(obj, "longValue", "()J", &[])?.j()?),
+                value: WasmValue::I64(env.call_method(jobject, "longValue", "()J", &[])?.j()?),
             })
-        } else if env.is_instance_of(obj, FLOAT_CLASS).unwrap_or(false) {
+        } else if env.is_instance_of(jobject, FLOAT_CLASS).unwrap_or(false) {
             Ok(Value {
-                value: WasmValue::F32(env.call_method(obj, "floatValue", "()F", &[])?.f()?),
+                value: WasmValue::F32(env.call_method(jobject, "floatValue", "()F", &[])?.f()?),
             })
-        } else if env.is_instance_of(obj, DOUBLE_CLASS).unwrap_or(false) {
+        } else if env.is_instance_of(jobject, DOUBLE_CLASS).unwrap_or(false) {
             Ok(Value {
-                value: WasmValue::F64(env.call_method(obj, "doubleValue", "()D", &[])?.d()?),
+                value: WasmValue::F64(env.call_method(jobject, "doubleValue", "()D", &[])?.d()?),
             })
         } else {
             Err(runtime_error(format!(
                 "Could not convert argument {:?} to a WebAssembly value.",
-                obj
+                jobject
             )))
         }
     }
