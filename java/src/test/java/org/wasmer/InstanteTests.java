3f236679912dd67a59b345e5130b4b3ea79bef2f
Asami Doi <doiasami1219@gmail.com>
2020-01-08 13:42:35 +0900

    Create a getBytes method to make each test simplify

--- java/src/test/java/org/wasmer/InstanteTests.java
+++ java/src/test/java/org/wasmer/InstanteTests.java
@@ -9,12 +9,14 @@ import java.nio.file.Paths;
 import org.junit.jupiter.api.Test;
 
 class InstanceTests {
+    private byte[] getBytes() throws IOException,Exception {
+        Path modulePath = Paths.get(getClass().getClassLoader().getResource("tests.wasm").getPath());
+        return Files.readAllBytes(modulePath);
+    }
+
     @Test
     void sum() throws IOException,Exception {
-        Path modulePath = Paths.get(getClass().getClassLoader().getResource("tests.wasm").getPath());
-        byte[] moduleBytes = Files.readAllBytes(modulePath);
-
-        Instance instance = new Instance(moduleBytes);
+        Instance instance = new Instance(getBytes());
 
         Integer[] arguments = {1, 2};
         assertEquals(3, (Integer) instance.call("sum", arguments)[0]);
@@ -24,10 +26,7 @@ class InstanceTests {
 
     @Test
     void i32_i32() throws IOException,Exception {
-        Path modulePath = Paths.get(getClass().getClassLoader().getResource("tests.wasm").getPath());
-        byte[] moduleBytes = Files.readAllBytes(modulePath);
-
-        Instance instance = new Instance(moduleBytes);
+        Instance instance = new Instance(getBytes());
 
         Integer[] arguments = {42};
         assertEquals(42, (Integer) instance.call("i32_i32", arguments)[0]);
@@ -37,10 +36,7 @@ class InstanceTests {
 
     @Test
     void i64_i64() throws IOException,Exception {
-        Path modulePath = Paths.get(getClass().getClassLoader().getResource("tests.wasm").getPath());
-        byte[] moduleBytes = Files.readAllBytes(modulePath);
-
-        Instance instance = new Instance(moduleBytes);
+        Instance instance = new Instance(getBytes());
 
         Long[] arguments = {42l};
         assertEquals(42l, (Long) instance.call("i64_i64", arguments)[0]);
@@ -50,10 +46,7 @@ class InstanceTests {
 
     @Test
     void f32_f32() throws IOException,Exception {
-        Path modulePath = Paths.get(getClass().getClassLoader().getResource("tests.wasm").getPath());
-        byte[] moduleBytes = Files.readAllBytes(modulePath);
-
-        Instance instance = new Instance(moduleBytes);
+        Instance instance = new Instance(getBytes());
 
         Float[] arguments = {42.0f};
         assertEquals(42.0f, (Float) instance.call("f32_f32", arguments)[0]);
@@ -63,10 +56,7 @@ class InstanceTests {
 
     @Test
     void f64_f64() throws IOException,Exception {
-        Path modulePath = Paths.get(getClass().getClassLoader().getResource("tests.wasm").getPath());
-        byte[] moduleBytes = Files.readAllBytes(modulePath);
-
-        Instance instance = new Instance(moduleBytes);
+        Instance instance = new Instance(getBytes());
 
         Double[] arguments = {42.0d};
         assertEquals(42.0d, (Double) instance.call("f64_f64", arguments)[0]);
@@ -76,10 +66,7 @@ class InstanceTests {
 
     @Test
     void i32_i64_f32_f64_f64() throws IOException,Exception {
-        Path modulePath = Paths.get(getClass().getClassLoader().getResource("tests.wasm").getPath());
-        byte[] moduleBytes = Files.readAllBytes(modulePath);
-
-        Instance instance = new Instance(moduleBytes);
+        Instance instance = new Instance(getBytes());
 
         Object[] arguments = {1, 2l, 3.0f, 4.0d};
         assertEquals(10.0d, (Double) instance.call("i32_i64_f32_f64_f64", arguments)[0]);
