5697f35c854195e40cff131737e30d8d182ce471
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2020-01-16 16:12:23 +0100

    doc(export) Improve documentation.

--- java/src/main/java/org/wasmer/Export.java
+++ java/src/main/java/org/wasmer/Export.java
@@ -6,8 +6,12 @@ import java.util.Map;
 import java.util.function.Function;
 
 /**
- * Functional interface for WebAssembly functions.
- * The apply method takes an arbitrary number of arguments and return an output.
+ * Functional interface for WebAssembly exported functions, i.e. it
+ * creates a new type for a closure that mimics a WebAssembly exported
+ * function.
+ *
+ * The apply method takes an arbitrary number of arguments and returns
+ * an output.
  */
 @FunctionalInterface
 interface ExportedFunction<Input, Output> {
