cfcc10d1044308f18273cd468d6b60aa5a1ff8dc
Asami Doi <doiasami1219@gmail.com>
2020-02-13 09:03:10 +0900

    doc: Add documentation to instance, module and exports object.

--- java/src/main/java/org/wasmer/Instance.java
+++ java/src/main/java/org/wasmer/Instance.java
@@ -1,11 +1,23 @@
 package org.wasmer;
 
+/**
+ * `Instance` is a Java class that represents a WebAssembly instance.
+ *
+ * # Examples
+ *
+ * ```java
+ * Instance instance = new Instance(wasmBytes);
+ * ```
+ */
 class Instance {
     private native long nativeInstantiate(Instance self, byte[] moduleBytes) throws RuntimeException;
     private native void nativeDrop(long instancePointer);
     protected native Object[] nativeCall(long instancePointer, String exportName, Object[] arguments) throws RuntimeException;
     protected static native void nativeInitializeExportedFunctions(long instancePointer);
 
+    /**
+     * All WebAssembly exported functions.
+     */
     public final Exports exports;
     protected long instancePointer;
 
@@ -13,6 +25,12 @@ class Instance {
         System.loadLibrary("java_ext_wasm");
     }
 
+    /**
+     * The constructor instantiates a new WebAssembly instance based on
+     * WebAssembly bytes.
+     *
+     * @param moduleBytes WebAssembly bytes.
+     */
     public Instance(byte[] moduleBytes) throws RuntimeException {
         this.exports = new Exports(this);
 
@@ -26,10 +44,17 @@ class Instance {
         this.exports = new Exports(this);
     }
 
+    /**
+     * Delete an instance object pointer.
+     */
     public void close() {
         this.nativeDrop(this.instancePointer);
     }
 
+    /**
+     * Delete an instance object pointer, which is called by the garbage collector
+     * before an object is removed from the memory.
+     */
     public void finalize() {
         this.close();
     }
--- java/src/main/java/org/wasmer/Module.java
+++ java/src/main/java/org/wasmer/Module.java
@@ -1,5 +1,17 @@
 package org.wasmer;
 
+/**
+ * `Module` is a Java class that represents a WebAssembly module.
+ *
+ * # Examples
+ *
+ * ```java
+ * boolean isValid = Module.validate(wasmBytes);
+ *
+ * Module module = new Module(wasmBytes);
+ * Instance instance = module.instantiate();
+ * ```
+ */
 class Module {
     private native long nativeModuleInstantiate(Module self, byte[] moduleBytes) throws RuntimeException;
     private native void nativeDrop(long modulePointer);
@@ -12,23 +24,47 @@ class Module {
         System.loadLibrary("java_ext_wasm");
     }
 
+    /**
+     * Check that given bytes represent a valid WebAssembly module.
+     *
+     * @param moduleBytes WebAssembly bytes.
+     * @return true if, and only if, given bytes are valid as a WebAssembly module.
+     */
     public static boolean validate(byte[] moduleBytes) {
         return Module.nativeValidate(moduleBytes);
     }
 
+    /**
+     * The constructor instantiates a new WebAssembly module based on
+     * WebAssembly bytes.
+     *
+     * @param moduleBytes WebAssembly bytes.
+     */
     public Module(byte[] moduleBytes) throws RuntimeException {
         long modulePointer = this.nativeModuleInstantiate(this, moduleBytes);
         this.modulePointer = modulePointer;
     }
 
+    /**
+     * Delete a module object pointer.
+     */
     public void close() {
         this.nativeDrop(this.modulePointer);
     }
 
+    /**
+     * Delete a module object pointer, which is called by the garbage collector
+     * before an object is removed from the memory.
+     */
     public void finalize() {
         this.close();
     }
 
+    /**
+     * Create an instance object based on a module object.
+     *
+     * @return Instance object.
+     */
     public Instance instantiate() {
         Instance instance = new Instance();
         long instancePointer = this.nativeInstantiate(this.modulePointer, instance);
