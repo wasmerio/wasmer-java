0e571f5953307d6117372814f48a29253837053f
Asami Doi <doiasami1219@gmail.com>
2020-02-17 22:41:41 +0900

    chore(memory): Force the memory order to little endian.

--- java/src/main/java/org/wasmer/Memory.java
+++ java/src/main/java/org/wasmer/Memory.java
@@ -1,6 +1,7 @@
 package org.wasmer;
 
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 class Memory {
     private ByteBuffer inner;
@@ -8,6 +9,9 @@ class Memory {
     private Memory() {
         // Initial size is 114112 bytes (65536 bytes (64 KiB) * 17 pages).
         this.inner = ByteBuffer.allocateDirect(1114112);
+        if (this.inner.order() != ByteOrder.LITTLE_ENDIAN) {
+            this.inner = this.inner.order(ByteOrder.LITTLE_ENDIAN);
+        }
     }
 
     public byte[] read(int offset, int length) {
